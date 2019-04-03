f33d0366cf133aad2136a9f758cd36deb420ff76
elisamartin <elisamv93@gmail.com>
2019-04-03 13:08:38 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile').development;
+
+const db = knex(knexConfig);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\nrunning on ${port}\n`));
