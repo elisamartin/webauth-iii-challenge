910279acc2c6b66f22192ce5300d567e5842c3ee
elisamartin <elisamv93@gmail.com>
2019-04-03 16:50:40 +0200

    MVP Day 1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const Users = require('./data/helpers/users-model');
+const protected = require('./middleware');
 
 const server = express();
 
@@ -15,33 +18,55 @@ server.get('/', (req, res) => {
 // [POST] /api/register
 server.post('/api/register', (req, res) => {
 	let user = req.body;
-	const hashPw = bcrypt.hashSync(user.password, 10);
-	user.password = hashPw;
+	const hash = bcrypt.hashSync(user.password, 10);
+	user.password = hash;
 
 	Users.add(user)
 		.then((saved) => {
 			res.status(201).json(saved);
 		})
-		.catch((err) => {
-			res.status(500).send(err);
+		.catch((error) => {
+			res.status(500).json({ message: 'Something went wrong' });
 		});
 });
+
+function makeToken(user) {
+	const payload = {
+		subject: user.id,
+		username: user.username
+	};
+	const options = {
+		expiresIn: '1h'
+	};
+	const token = jwt.sign(payload, secret, options);
+	return token;
+}
 
 // [POST] /api/login
 server.post('/api/login', (req, res) => {
 	let { username, password } = req.body;
-	Users.findBy({ username }).first().then((user) => {
-		if (users && bcrypt.compareSync(password, users.password)) {
-			res.json({ info: 'Logged in' });
-		} else {
-			res.status(404).json({ message: 'You shall not pass!' });
-		}
-	});
+
+	Users.findBy({ username })
+		.first()
+		.then((user) => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = makeToken(user);
+				res.status(200).json({
+					message: `Hi ${user.username}!`,
+					token
+				});
+			} else {
+				res.status(401).json({ message: 'Invalid Credentials' });
+			}
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
 // [GET] /api/users
-server.get('/api/users', (req, res) => {
-	Users.getUsers('users')
+server.get('/api/users', protected, (req, res) => {
+	Users.find('users')
 		.select('id', 'username')
 		.then((users) => {
 			res.json(users);
