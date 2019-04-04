d292e63c650ace202c4471126386c888a9008079
elisamartin <elisamv93@gmail.com>
2019-04-04 17:09:24 +0200

    Register

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, NavLink, withRouter } from 'react-router-dom';
 import Login from './components/Login';
 import Users from './components/Users';
+import Register from './components/Register';
 
 class App extends Component {
 	logout = () => {
@@ -15,11 +16,13 @@ class App extends Component {
 				<nav>
 					<NavLink to="/">Login</NavLink>
 					<NavLink to="/users">Users</NavLink>
+					<NavLink to="/register">Register</NavLink>
 					<button onClick={this.logout}>Logout</button>
 				</nav>
 				<div>
 					<Route exact path="/" component={Login} />
 					<Route exact path="/users" component={Users} />
+					<Route exact path="/register" component={Register} />
 				</div>
 			</div>
 		);
